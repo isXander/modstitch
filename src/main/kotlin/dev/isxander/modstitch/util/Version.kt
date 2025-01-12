e28dff727f9f25a1f2f8c59ee85978528df3bbb5
isXander <xander@isxander.dev>
2025-01-12 17:40:25 +0000

    print version on every project

--- src/main/kotlin/dev/isxander/modstitch/util/Version.kt
+++ src/main/kotlin/dev/isxander/modstitch/util/Version.kt
@@ -6,9 +6,5 @@ import org.gradle.api.Project
 val MODSTITCH_VERSION: String = ModstitchExtensionPlugin::class.java.`package`.implementationVersion
 
 fun printVersion(suffix: String, project: Project) {
-    val prop = "modstitch$suffix-versionprinted"
-    if (project.rootProject.hasProperty(prop)) return
-    project.rootProject.extensions.extraProperties[prop] = true
-
     project.logger.lifecycle("Modstitch/$suffix $MODSTITCH_VERSION")
 }
