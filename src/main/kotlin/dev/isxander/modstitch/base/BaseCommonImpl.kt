47aed90bfaaf688d0d1020c36ee4c33e5401ed15
Kira-NT <kira.canary@proton.me>
2025-06-28 18:48:19 +0100

    Set `accessWideners` input for `appendModMetadata`

--- src/main/kotlin/dev/isxander/modstitch/base/BaseCommonImpl.kt
+++ src/main/kotlin/dev/isxander/modstitch/base/BaseCommonImpl.kt
@@ -93,6 +93,7 @@ abstract class BaseCommonImpl<T : Any>(
 
             source(target.mainSourceSet!!.output.resourcesDir!!.resolve(msExt.modLoaderManifest))
             mixins.value(target.provider { msExt.mixin.configs.map { it.resolved() } })
+            accessWideners.value(msExt.accessWidenerName.zip(msExt.accessWidener) { n, _ -> listOf(n) }.orElse(listOf()))
         }.also { target.tasks["processResources"].finalizedBy(it) }
     }
 
